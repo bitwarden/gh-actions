b0745adbeb94a71f225fd0a71b35511b422e1767
Micaiah Martin <mmartin@bitwarden.com>
2021-11-29 12:33:05 -0700

    Added version bump code and tests

---
+++ version-bump/main.py
@@ -0,0 +1,66 @@
+import os
+import json
+import plistlib
+import lxml.etree as ET
+
+def get_file_type(file):
+    file_type = os.path.splitext(file)[1]
+    return file_type
+
+def update_json(version, file):
+    with open(file) as json_file:
+        data = json.load(json_file)
+        data["version"] = version
+        try:
+            data["packages"][""]["version"] = version
+        except KeyError:
+            pass
+        json.dump(data, open(file, "w"), indent=2)
+
+
+def update_plist(version, file):
+    with open(file, "rb") as plist:
+
+        pl = plistlib.load(plist)
+        pl["CFBundleShortVersionString"] = version
+
+        data = pl
+    with open(file, "wb") as update_plist:
+        plistlib.dump(data, update_plist, sort_keys=False)
+
+
+def update_xml(version, file):
+    mytree = ET.parse(file)
+    myroot = mytree.getroot()
+    myroot.attrib[
+        "{http://schemas.android.com/apk/res/android}versionName"
+        ] = version
+    ET.register_namespace(
+        "android", "http://schemas.android.com/apk/res/android")
+    ET.register_namespace(
+        "tools", "http://schemas.android.com/tools")
+    mytree.write(
+        file, encoding="utf-8", xml_declaration=True, pretty_print=True)
+
+
+if __name__ == "__main__":
+    version = os.getenv("INPUT_VERSION")
+    file_path = os.getenv("INPUT_FILE_PATH")
+
+    try:
+        os.path.isfile(file_path)
+    except TypeError:
+        raise Exception(f"File path for {file_path} not found.")
+
+    file_type = get_file_type(file_path)
+
+    if file_type == ".xml":
+        update_xml(version, file_path)
+    elif file_type == ".json":
+        update_json(version, file_path)
+    elif file_type == ".plist":
+        update_plist(version, file_path)
+    else:
+        raise Exception("No input values provided.")
+
+    print(f"::set-output name=status::Updated {file_path}")

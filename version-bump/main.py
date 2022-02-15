ae90937a83e78389727ca2c08f62801b1873941c
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2022-02-15 14:06:51 -0500

    Update variable names

--- version-bump/main.py
+++ version-bump/main.py
@@ -17,37 +17,37 @@ def get_file_name(file_path):
     return file_name
 
 
-def update_json(version, file):
-    with open(file) as json_file:
+def update_json(version, file_path):
+    with open(file_path) as json_file:
         data = json.load(json_file)
         data["version"] = version
         try:
             data["packages"][""]["version"] = version
         except KeyError:
             pass
-        json.dump(data, open(file, "w"), indent=2)
-    with open(file, "a") as f:
+        json.dump(data, open(file_path, "w"), indent=2)
+    with open(file_path, "a") as f:
         f.write("\n")  # Make sure we add the new line back in at EOF.
 
 
-def update_plist(version, file):
+def update_plist(version, file_path):
     with open(file, "rb") as plist:
 
         pl = plistlib.load(plist)
         pl["CFBundleShortVersionString"] = version
 
         data = pl
-    with open(file, "wb") as update_plist:
+    with open(file_path, "wb") as update_plist:
         plistlib.dump(data, update_plist, sort_keys=False)
 
 
-def update_xml(version, file):
-    mytree = ET.parse(file)
+def update_xml(version, file_path):
+    mytree = ET.parse(file_path)
     myroot = mytree.getroot()
 
     # Android Manifests
     if myroot.tag == "manifest":
-        with open(file, "r") as f:
+        with open(file_path, "r") as f:
             data = f.read()
             data_new = re.sub(
                 'android:versionName="[0-9]+\.[0-9]+\.[0-9]+"',
@@ -55,28 +55,28 @@ def update_xml(version, file):
                 data,
                 flags=re.M,
             )
-        with open(file, "w") as f:
+        with open(file_path, "w") as f:
             f.write(data_new)
 
     # Microsoft .NET project files
     elif myroot.attrib.has_key("Sdk") and "Microsoft.NET.Sdk" in myroot.attrib["Sdk"]:
         version_property = [x for x in myroot[0] if x.tag == "Version"][-1]
         version_property.text = version
-        mytree.write(file)
+        mytree.write(file_path)
     # MSBuild Props
     else:
         myroot[0][1].text = version
-        mytree.write(file, encoding="utf-8")
+        mytree.write(file_path, encoding="utf-8")
 
 
 # For updating Helm Charts - Chart.yaml version
-def update_yaml(version, file):
-    with open(file, "r") as f:
+def update_yaml(version, file_path):
+    with open(file_path, "r") as f:
         doc = yaml.load(f, Loader=yaml.FullLoader)
 
     doc["version"] = version
 
-    with open(file, "w") as f:
+    with open(file_path, "w") as f:
         yaml.dump(doc, f)
 
 

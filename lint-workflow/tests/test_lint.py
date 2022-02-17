676ae4100b19625c0b0b70525e6c0181bd2d405f
Micaiah Martin <77340197+mimartin12@users.noreply.github.com>
2022-02-17 08:43:40 -0600

    call-workflow support added (#29)

--- lint-workflow/tests/test_lint.py
+++ lint-workflow/tests/test_lint.py
@@ -4,5 +4,5 @@ def test_lint(capfd):
     file_path = "test.yml"
     lint_output = lint(file_path)
     out, err = capfd.readouterr()
-    assert "Step 1 of job key 'download-latest' uses an outdated action, consider updating it" in out
-    assert "Run in step 2 of job key 'download-latest' should be a single line." in out
+    assert "Step 1 of job key 'test-normal-action' uses an outdated action, consider updating it" in out
+    assert "Run in step 2 of job key 'test-normal-action' should be a single line." in out

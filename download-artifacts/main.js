29bbba1887a5f183d15cbf389ec95c425599460f
Joseph Flinn <joseph.s.flinn@gmail.com>
2022-04-14 20:44:26 +0000

    fixing variable name

--- download-artifacts/main.js
+++ download-artifacts/main.js
@@ -74,7 +74,7 @@ async function main() {
                 repo: repo,
                 workflow_id: workflow
             }).then(workflowRunsResponse => {
-                console.log(`Type of runs: ${typeof(runs)}\nruns: ${JSON.stringify(runs)}`)
+                console.log(`Type of runs: ${typeof(workflowRunsResponse)}\nruns: ${JSON.stringify(workflowRunsResponse)}`)
                 return workflowRunsResponse.data.workflow_runs.filter(run => run.head_branch == branch)
                 .sort((a, b) => {
                     a_date = new Date(a.created_at)

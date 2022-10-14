bd1f57d01e7b25ab1d0980c91014f25872399173
Joseph Flinn <joseph.s.flinn@gmail.com>
2022-10-14 13:44:24 -0700

    Expanding test_action_update to include a test case for the new memoization

--- lint-workflow/tests/test_action_update.py
+++ lint-workflow/tests/test_action_update.py
@@ -1,6 +1,6 @@
 import urllib3 as urllib
 
-from lint import get_action_update
+from lint import get_action_update, memoized_action_update_urls
 
 http = urllib.PoolManager()
 
@@ -14,3 +14,5 @@ def test_action_update():
     r = http.request("GET", update_url)
 
     assert r.status == 200
+
+    assert "actions/checkout" in memoized_action_update_urls

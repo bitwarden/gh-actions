1c86173bb1cb0046002f150138cd9199b2548989
Micaiah Martin <github@sourcecodemt.com>
2022-11-21 15:39:41 -0700

    Add Dagger CI for linter workflow

---
+++ lint-workflow/ci/test.py
@@ -0,0 +1,35 @@
+import sys
+import anyio
+import dagger
+
+
+async def test():
+    config = dagger.Config(log_output=sys.stderr)
+
+    async with dagger.Connection(config) as client:
+
+        # get reference to the local project
+        src_id = await client.host().directory("lint-workflow").id()
+
+        python = (
+            client.container()
+            # pull python container
+            .from_("python:3.10-slim-buster")
+            # mount cloned repository into image
+            .with_mounted_directory("/src", src_id)
+            # set working directory
+            .with_workdir("/src")
+            # install test dependencies
+            .exec(["pip", "install", "pipenv"]).exec(["pipenv", "install", "-d"])
+            # run tests
+            .exec(["pipenv", "run", "pytest", "tests/"])
+        )
+
+        # execute
+        await python.exit_code()
+
+        print("Tests Successful")
+
+
+if __name__ == "__main__":
+    anyio.run(test)

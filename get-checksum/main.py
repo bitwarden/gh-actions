1a9a726c6caa14fd33243477d8a996a629b6f0e2
Michal Checinski <mchecinski@bitwarden.com>
2023-01-04 15:43:46 +0100

    Add messages for listing input

--- get-checksum/main.py
+++ get-checksum/main.py
@@ -13,13 +13,11 @@ if __name__ == "__main__":
     packages_dir = os.getenv("INPUT_PACKAGES_DIR")
     file_path = os.getenv("INPUT_FILE_PATH")
 
-    # if os.path.isdir(file_path):
-    #     file_path = os.path.join(file_path, "sha256-checksums.txt")
-    # elif not os.path.isfile(file_path):
-    #     raise Exception("file_path input must be directory or full file path.")
+    if os.path.isdir(file_path):
+        file_path = os.path.join(file_path, "sha256-checksums.txt")
 
-    # if not os.path.isdir(packages_dir):
-    #     raise Exception("packages_dir input must be directory.")
+    print(f"Packages directory: {packages_dir}")
+    print(f"File which will contain SHA256 checksums will be: {file_path}")
 
     hashes = ""
 

fcff8aaa065317a24e17943e32df993fa2f5f0ec
Opeyemi <54288773+Eeebru@users.noreply.github.com>
2023-04-27 11:35:30 +0100

    [DEVOPS-1323] add linter for action version pin (#114)
    * add linter for action version pin
    
    * update run step for pinned version check
    
    * update script
    
    * Test the check pinned version
    
    * update version pin check step
    
    * Format script
    
    * update workflow linter (WIP)
    
    * update workflow linter: use ruamel library (WIP)
    
    * update workflow linter (revert to using bash script)
    
    * Add empty var check
    
    * Add comments

---
+++ .github/version-lint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+MISSING_VERSION_FILES=""
+# Changed files input and trim with new line
+FILES_TO_CHANGE=$(tr ' ' '\n' <<< "$1")
+
+# Read each changed filenames from the input
+while IFS= read -r line; do
+  # Grep for lines with 'uses' and not having version tag
+  VERSION_PATTERN=$(cat "${line}" | grep 'uses: ' | grep -vE '#\s+v[0-9.]+(\s|$)')
+
+  # Check if $VERSION_PATTERN is not empty
+  if [[ ! -z "$VERSION_PATTERN" ]]; then
+    # Read each line that does not have version tag
+    while IFS= read -r each_line; do
+      # if the line does contain bitwarden/gh-actions
+      if ! grep -qE 'bitwarden/gh-actions/*' <<< $each_line ; then
+        # Add that filename to the variable storing files with missing actions version
+        MISSING_VERSION_FILES+=" ${line} "
+      fi
+    done <<< "$VERSION_PATTERN"
+  fi
+done <<< "$FILES_TO_CHANGE"
+
+# Trim the variable to be a single line
+MISSING_FILES=$(echo $MISSING_VERSION_FILES | tr '\n' ' ')
+# Check if the variable is not empty
+if [ -n "$MISSING_VERSION_FILES" ]; then
+  echo "### :mega: Workflow files ${MISSING_FILES} are missing actions version tag" >> $GITHUB_STEP_SUMMARY
+  exit 1
+fi

de28c805e9dfa3f495ee5820dbf73e784c9d1b8a
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2023-05-23 13:03:46 -0400

    Update version-bump action to remove 'set-output' deprecated command

--- version-bump/main.py
+++ version-bump/main.py
@@ -105,4 +105,4 @@ if __name__ == "__main__":
     else:
         raise Exception("No file was recognized as a supported format.")
 
-    print(f"status=Updated {file_path} >> $GITHUB_OUTPUT")
+    os.environ["GITHUB_OUTPUT"] += f"status=Updated {file_path}"

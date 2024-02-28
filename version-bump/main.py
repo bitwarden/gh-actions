9bde7228f5111b281c1b54b6b16eeb392f6fe17c
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2024-02-28 14:12:35 -0500

    Update version-bump action

--- version-bump/main.py
+++ version-bump/main.py
@@ -1,9 +1,20 @@
-import os
+from datetime import date
 import json
+import lxml.etree as ET
+import os
 import plistlib
 import re
-import lxml.etree as ET
 import yaml
+
+
+def get_next_version(version):
+    version_split = version.split('.')
+    year = int(version_split[0])
+    month = int(version_split[1])
+    patch = int(version_split[2])
+    current_date = date(date.today().year, date.today().month, 1)
+    patch = 0 if year != current_date.year or month != current_date.month else patch + 1
+    return f"{current_date.year}.{current_date.month}.{patch}"
 
 
 def get_file_type(file_path):
@@ -16,12 +27,12 @@ def get_file_name(file_path):
     return file_name
 
 
-def update_json(version, file_path):
+def update_json(file_path, version=None):
     with open(file_path) as json_file:
         data = json.load(json_file)
-        data["version"] = version
+        data["version"] = version if version is not None else get_next_version(data["version"])
         try:
-            data["packages"][""]["version"] = version
+            data["packages"][""]["version"] = version if version is not None else get_next_version(data["packages"][""]["version"])
         except KeyError:
             pass
         json.dump(data, open(file_path, "w"), indent=2)
@@ -29,28 +40,29 @@ def update_json(version, file_path):
         f.write("\n")  # Make sure we add the new line back in at EOF.
 
 
-def update_plist(version, file_path):
+def update_plist(file_path, version=None):
     with open(file_path, "rb") as plist:
 
         pl = plistlib.load(plist)
-        pl["CFBundleShortVersionString"] = version
+        pl["CFBundleShortVersionString"] = version if version is not None else get_next_version(pl["CFBundleShortVersionString"])
 
         data = pl
     with open(file_path, "wb") as update_plist:
         plistlib.dump(data, update_plist, sort_keys=False)
 
 
-def update_xml(version, file_path):
+def update_xml(file_path, version=None):
     mytree = ET.parse(file_path)
     myroot = mytree.getroot()
 
     # Android Manifests
     if myroot.tag == "manifest":
+        new_version = version if version is not None else get_next_version(myroot.attrib.get('{http://schemas.android.com/apk/res/android}versionName'))
         with open(file_path, "r") as f:
             data = f.read()
             data_new = re.sub(
                 'android:versionName="[0-9]+\.[0-9]+\.[0-9]+"',
-                f'android:versionName="{version}"',
+                f'android:versionName="{new_version}"',
                 data,
                 flags=re.M,
             )
@@ -60,21 +72,21 @@ def update_xml(version, file_path):
     # Microsoft .NET project files
     elif myroot.attrib.has_key("Sdk") and "Microsoft.NET.Sdk" in myroot.attrib["Sdk"]:
         version_property = [x for x in myroot[0] if x.tag == "Version"][-1]
-        version_property.text = version
+        version_property.text = version if version is not None else get_next_version(version_property.text)
         mytree.write(file_path)
     # MSBuild Props
     else:
         version_property = [x for x in myroot[0] if x.tag == "Version"][-1]
-        version_property.text = version
+        version_property.text = version if version is not None else get_next_version(version_property.text)
         mytree.write(file_path, encoding="utf-8")
 
 
 # For updating Helm Charts - Chart.yaml version
-def update_yaml(version, file_path):
+def update_yaml(file_path, version=None):
     with open(file_path, "r") as f:
         doc = yaml.load(f, Loader=yaml.FullLoader)
 
-    doc["version"] = version
+    doc["version"] = version if version is not None else get_next_version(doc["version"])
 
     with open(file_path, "w") as f:
         yaml.dump(doc, f)
@@ -95,13 +107,13 @@ if __name__ == "__main__":
 
     # Handle the file based on the extension.
     if file_type in {".xml", ".props", ".csproj"}:
-        update_xml(version, file_path)
+        update_xml(file_path, version)
     elif file_type == ".json":
-        update_json(version, file_path)
+        update_json(file_path, version)
     elif file_type == ".plist":
-        update_plist(version, file_path)
+        update_plist(file_path, version)
     elif file_name == "Chart.yaml" or file_name == "Chart.yml":
-        update_yaml(version, file_path)
+        update_yaml(file_path, version)
     else:
         raise Exception("No file was recognized as a supported format.")
 

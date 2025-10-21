44b227da4a134f133f9ba64edc3dafdb5e88145d
Andy Pixley <3723676+pixman20@users.noreply.github.com>
2025-10-21 12:39:21 -0400

    [BRE-1264] Only use colors if supported

--- version-bump/test-local.sh
+++ version-bump/test-local.sh
@@ -5,11 +5,18 @@
 
 set -e
 
-# Color output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+# Color output (only if terminal supports it)
+if [[ -t 1 ]]; then
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[1;33m'
+    NC='\033[0m'
+else
+    RED=''
+    GREEN=''
+    YELLOW=''
+    NC=''
+fi
 
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
